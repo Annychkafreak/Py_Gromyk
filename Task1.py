f6136a70bad74f101533d6fb41ae35f78a523f41
Annychkafreak <jabeniatko@gmail.com>
2021-01-11 01:39:49 +0200

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,15 @@
+string = "2, -1, 9, 6"
+    list = string
+        map_object = map(int, list)
+     list_of_int = list
+print(list)
+
+if __name__ == "__main__":
+     list_of_int.sum = sum(list_of_int)
+        print (sum(list))
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,18 @@
+def print_dictionary(e2g):
+    "Use a breakpoint in the code line below to debug your script."
+    print(f'dictionary, {e2g}')
+
+
+thisdict = {
+  "stork": "storch",
+  "hawk": "falke",
+  "woodpecker": "specht",
+  "owl": "eule"
+}
+x = thisdict.values()
+print(x)
+
+if __name__ == '__main__':
+    print ("e2g")
+
+
