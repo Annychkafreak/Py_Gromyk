20527e91301f8a05a1117d9940f438a70bf90af3
Annychkafreak <jabeniatko@gmail.com>
2021-01-11 13:22:05 +0200

    Add files via upload

---
+++ Dict_rivers.py
@@ -0,0 +1,10 @@
+my_dict = {'river':'Amazon','country':'South America'}
+      my_dict = {'river': 'The Danube', 'country':'Central and Eastern Europe'}
+       my_dict = {'river': 'Mississippi', 'country': 'The US, Canada'}
+        my_dict = {'river':'Yukon', 'country':'North America'}
+          my_dict = {'river': 'The Dnieper', 'country': 'Ukraine'}
+
+    if __name__ == “__main__”
+     print(my_dict['river'] runs through['country'])
+)
+
